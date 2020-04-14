258527dd65abe30d0b76ed70a0edee8662ca8946
MadhushiADL <madhushi.welikala@axiatadigitallas.com>
2020-04-15 00:41:46 +0530

    image base64 conversion

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-$(document).ready(function(){
-
-    $.ajax({
-             url: "http://www.mocky.io/v2/5e94c83a31000055bf5e34ec",
-             type: 'GET',
-             dataType: 'json',
-             success: function(res) {
-                $('#result').html(res)
-             }
-         });
- 
- })
-
-
- 
-/*  ==========================================
-    SHOW UPLOADED IMAGE
-* ========================================== */
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
             $('#imageResult')
                 .attr('src', e.target.result);
+            var b64 = reader.result.replace(/^data:.+;base64,/, '');
+            // $(document).ready(function(){
+            //     $.ajax({
+            //         url: "http://localhost:8088/ant/details",
+            //         data: b64,
+            //         type: 'GET',
+            //         dataType: 'json',
+            //         success: function(res) {
+            //         $('#result').html(res)
+            //         }
+            //     }); 
+            // })
         };
         reader.readAsDataURL(input.files[0]);
         $('#btnClassify').prop('disabled', false);
